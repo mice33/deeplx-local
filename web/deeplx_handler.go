a68734f92129df699a7fb64d2939af9637ffb740
mice <mice33@gmail.com>
2024-07-02 14:38:06 +0800

    feat: 允许跨域请求

--- web/deeplx_handler.go
+++ web/deeplx_handler.go
@@ -21,8 +21,18 @@ func NewDeepLXHandler(service service.TranslateService, customRoute string) *Dee
 	}
 	return &DeepLXHandler{service: service, routePath: customRoute}
 }
-
 func (d *DeepLXHandler) Translate(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "POST")
+	c.Header("Access-Control-Allow-Headers", "Content-Type")
+
+	// Handle preflight request
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
+
 	var request domain.TranslateRequest
 	if err := c.ShouldBindJSON(&request); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -34,5 +44,6 @@ func (d *DeepLXHandler) Translate(c *gin.Context) {
 }
 
 func (d *DeepLXHandler) RegisterRoutes(engine *gin.Engine) {
+	engine.OPTIONS(d.routePath, d.Translate)
 	engine.POST(d.routePath, d.Translate)
 }
